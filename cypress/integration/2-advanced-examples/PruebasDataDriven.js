5d9d86ceb1ca4b64fc42500c503efa0d51662866
Gaston Cordeiro <gaston.cordeiro@darwoft.com>
2021-09-24 16:53:01 -0300

    Formulario Date of Birth

--- cypress/integration/2-advanced-examples/PruebasDataDriven.js
+++ cypress/integration/2-advanced-examples/PruebasDataDriven.js
@@ -19,10 +19,19 @@ describe('Segundo conjunto de casos de prueba avanzadas', function(){
         cy.get('#firstName').type(this.datos.name)
         cy.get('#lastName').type(this.datos.lastName)
         cy.get('#userEmail').type(this.datos.email) 
-        cy.get('input[name="gender"][value="Female"]').check({force: true})
+        cy.get('input[name="gender"][value="'+ this.datos.gender +'"]').check({force: true}).should('be.checked')
     
         cy.get('#userNumber').type(this.datos.mobile) 
+        cy.get('#dateOfBirthInput').click()
+        cy.get('.react-datepicker__month-select').should('be.visible').select(this.datos.dateOfBirth[0])
+        
+        cy.get('.react-datepicker__year-select').should('be.visible').select(this.datos.dateOfBirth[1])
+        cy.get('.react-datepicker__day--0' + this.datos.dateOfBirth[2]).should('be.visible').click()
         cy.get('#dateOfBirthInput')
+            .should('contain.value', this.datos.dateOfBirth[0].substring(0,3))
+            .should('contain.value', this.datos.dateOfBirth[1])
+            .should('contain.value', this.datos.dateOfBirth[2])
+        
         cy.get('#subjectsContainer > div').type(this.datos.mobile) 
         
     })
